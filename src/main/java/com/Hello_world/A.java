dc708559e76508b93655d88b0fb5914a0750ec36
satya918 <satyamohanty918@gmail.com>
2023-08-16 14:37:32 +0530

    msg

---
+++ src/main/java/com/Hello_world/A.java
@@ -0,0 +1,16 @@
+package com.Hello_world;
+
+public class A {
+	 public static void main(String[] args) {
+	        String[] months = {
+	            "January", "February", "March", "April", "May", "June",
+	            "July", "August", "September", "October", "November", "December"
+	        };
+
+	        System.out.println("Months of the year:");
+	        for (String month : months) {
+	            System.out.println(month);
+	        }
+
+	 }
+}
