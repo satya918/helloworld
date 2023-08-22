02f74b3d553727639a53906c525bfb8c0b29bfc9
Satya <121515890+satya918@users.noreply.github.com>
2023-08-22 17:24:05 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/Hello_world/HelloWorldApplication.java
+++ src/main/java/com/Hello_world/HelloWorldApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(Application.class);
+	   }
 
 }
